d76fb8b91cbb08eae11911e79d06c20258ae7220
DimitriosMalonas <dimitriosmalonas@outlook.com>
2019-12-09 18:24:13 +0000

    Created the architecture of the game. Game class that contains Enemy, and Tower class, and Rat and Elephant classes that inherit from Enemy, as well as Slingshot and Catapult classes that inherit from Tower.

---
+++ TowerDefenceGame/src/towerDefence/Game.java
@@ -0,0 +1,27 @@
+package towerDefence;
+
+import java.util.ArrayList;
+
+public class Game {
+	private ArrayList<Enemy> enemies;
+	private ArrayList<Tower> towers;
+	
+	private int corridorLength = 0;
+	
+	public Game(int corridorLengthIn) {
+		this.corridorLength = corridorLengthIn; 
+	}
+	
+	public void advance() {
+		
+	}
+	
+	public int getCorridorLength() {
+		return corridorLength;
+	}
+
+	public void setCorridorLength(int corridorLength) {
+		this.corridorLength = corridorLength;
+	}
+
+}
